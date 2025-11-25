e7c671ec30eb4329fdc092dd16f251f396c85f7f
urmimondal001 <urmim056@gmail.com>
2025-11-25 13:27:27 +0530

    Update reelsRoute.js

--- server/utils/server/utils/server/controllers/server/routes/reelsRoute.js
+++ server/utils/server/utils/server/controllers/server/routes/reelsRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getReels, uploadReel } = require("../controllers/reelsController");
+const { getReels, uploadReel, likeReel, commentReel } = require("../controllers/reelsController");
 
 router.get("/", getReels);
 router.post("/upload", uploadReel);
+router.post("/like", likeReel);
+router.post("/comment", commentReel);
 
 module.exports = router;
