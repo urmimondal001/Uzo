ed38970dd05c6d405ccc9cd31643f37eac333ce8
urmimondal001 <urmim056@gmail.com>
2025-12-09 09:52:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import signupRoute from "./sign-up.js";
 import loginRoute from "./login.js";
+import forgotPasswordRoute from "./forgot-password.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// routes
+// Routes
 app.use("/api", signupRoute);
 app.use("/api", loginRoute);
+app.use("/api", forgotPasswordRoute);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
