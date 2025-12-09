dabc4fa4647d145175e6a3ba6775f767d3e0cfa5
urmimondal001 <urmim056@gmail.com>
2025-12-09 11:35:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ import likeRoute from "./routes/like-reel.js";
 import viewRoute from "./routes/view-reel.js";
 import commentRoute from "./routes/comment-reel.js";
 import deleteReelRoute from "./routes/delete-reel.js";
-import followRoute from "./routes/follow.js"; // 
+import followRoute from "./routes/follow.js";
+import shareReelRoute from "./routes/share-reel.js"; // <-- New
 
 const app = express();
 
@@ -25,7 +26,8 @@ app.use("/api", likeRoute);
 app.use("/api", viewRoute);
 app.use("/api", commentRoute);
 app.use("/api", deleteReelRoute);
-app.use("/api", followRoute); // 
+app.use("/api", followRoute);
+app.use("/api", shareReelRoute); // <-- New
 
 // Default
 app.get("/", (req, res) => {
