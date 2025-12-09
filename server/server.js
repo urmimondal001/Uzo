6b6998a446b0c16157262aba69fcdb901344bd98
urmimondal001 <urmim056@gmail.com>
2025-12-09 09:40:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const reelsRoute = require("./routes/reelsRoute");
+import express from "express";
+import cors from "cors";
+import signupRoute from "./sign-up.js";
 
 const app = express();
+
 app.use(cors());
-app.use(express.json({ limit: "100mb" })); // large video support
+app.use(express.json());
 
-app.use("/api/reels", reelsRoute);
+// Sign up route
+app.use("/api", signupRoute);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
