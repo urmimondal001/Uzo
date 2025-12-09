f37898c476454791735d7879258a39283d97beca
urmimondal001 <urmim056@gmail.com>
2025-12-09 10:00:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 import express from "express";
 import cors from "cors";
+
+// Import Routes
 import signupRoute from "./sign-up.js";
 import loginRoute from "./login.js";
 import forgotPasswordRoute from "./forgot-password.js";
+import likeRoute from "./like-reel.js";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api", signupRoute);
-app.use("/api", loginRoute);
-app.use("/api", forgotPasswordRoute);
+// API Routes
+app.use("/api", signupRoute);         // Sign Up
+app.use("/api", loginRoute);          // Log In
+app.use("/api", forgotPasswordRoute); // Forgot Password
+app.use("/api", likeRoute);           // Like System
 
+// Default
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Start Server
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
